ca83674b3c834fcf52f72028372ce88b9ea906c5
samsak1990 <samsak.developer@internet.ru>
2024-12-17 23:09:34 +0300

    A little fix

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderComp from "../views/HeaderComp";
 import FooterComp from "../views/FooterComp";
+import Steps from "./Steps";
 
 const Main = ()=>{
     return(
         <>
             <HeaderComp></HeaderComp>
+            <Steps></Steps>
             <FooterComp></FooterComp>
         </>
         
---
+++ src/components/pages/Steps.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Steps = ()=>{
+    return (
+        <h1>PAGE STEPS</h1>
+    )
+}
+
+export default Steps
