0cf0a16b67c7dd7e2e7d131bdbbccb21796ffa0f
samsak1990 <samsak.developer@internet.ru>
2024-12-18 23:09:40 +0300

    Added page 404

--- src/components/pages/Page404.jsx
+++ src/components/pages/Page404.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Page404 = ()=>{
     return (
-        <>
-            <h1>NOT FAUND</h1>
-        </>
+        <div style={{textAlign:'center'}}>
+            <h1 style={{fontSize: '72px'}}>ERROR <span style={{color: 'red'}}>404</span>!</h1>
+            <h2 style={{fontSize: '40px'}}>Page not found</h2>
+        </div>
     )
 }
 
